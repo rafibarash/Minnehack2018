36c5c8109380ffa32348171e3a36610637cc1944
Rafi Barash <rafabarash@gmail.com>
2018-01-28 01:57:55 -0600

    allowed host changes

--- minnehack2018/settings.py
+++ minnehack2018/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['morning-wildwood-94065.herokuapp.com', ]
+ALLOWED_HOSTS = ['morning-wildwood-94065.herokuapp.com', '127.0.0.1'] #config('ALLOWED_HOSTS')
 
 
 # Application definition
