a7609313542e7e9dc1be6b385b113bc3335651e1
Rafi Barash <rafabarash@gmail.com>
2018-01-28 08:01:04 -0600

    small change to base_time

--- concussiontest/models.py
+++ concussiontest/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Athlete(models.Model):
     name = models.CharField(max_length=30, default='')
     slug = models.SlugField(max_length=50, default='', unique=True)
-    base_time = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    base_time = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     base_errors = models.IntegerField(default=0)
