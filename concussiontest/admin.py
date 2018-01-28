9f538e977eb6c349081d60adba9923949a781569
Rafi Barash <rafabarash@gmail.com>
2018-01-28 07:54:17 -0600

    updated fields of athlete model

--- concussiontest/admin.py
+++ concussiontest/admin.py
@@ -3,6 +3,6 @@ from .models import Athlete
 
 class AthleteAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ('name', 'slug', 'base_time', 'new_time_1', 'new_time_2')
+    list_display = ('name', 'slug', 'base_time', 'base_errors')
     search_fields = ['name']
 admin.site.register(Athlete, AthleteAdmin)
