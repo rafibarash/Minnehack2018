3f6614cd1741b3dbe5deb7a82b153ea699aeebd6
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-28 07:37:26 -0600

    Got rid of radio buttons for test in JS

--- concussiontest/static/js/grayscale.js
+++ concussiontest/static/js/grayscale.js
@@ -3,53 +3,23 @@
 (function() {
   const myQuestions = [
     {
-      question: "Did the player lose consciousness or are they unresponsive?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
+      question: "Did the player lose consciousness or are they unresponsive?"
     },
     {
-      question: "Does player display any disorientation or inability to respond appropriately to questions?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
+      question: "Does player display any disorientation or inability to respond appropriately to questions?"
     },
     {
-      question: "Does the player display any signs of retrograde or anterograde amnesia?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
+      question: "Does the player display any signs of retrograde or anterograde amnesia?"
     },
     {
-      question: "Does the player display any new or persistent symptoms?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
+      question: "Does the player display any new or persistent symptoms?"
     },
     {
-      question: "Does the player display any abnormal neurological finding?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
+      question: "Does the player display any abnormal neurological finding?"
     },
     {
-      question: "Does the player display progressive, persistent or worsening symptoms?",
-      answers: {
-        a: "Yes",
-        b: "No"
-      },
-      correctAnswer: "b"
-    },
+      question: "Does the player display progressive, persistent or worsening symptoms?"
+    }
   ];
 
   function buildQuiz() {
@@ -59,25 +29,11 @@
     // for each question...
     myQuestions.forEach((currentQuestion, questionNumber) => {
       // we'll want to store the list of answer choices
-      const answers = [];
-
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-             <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-           </label>`
-        );
-      }
-
+      
       // add this question and its answers to the output
       output.push(
         `<div class="slide">
            <div class="question"> ${currentQuestion.question} </div>
-           <div class="answers"> ${answers.join("")} </div>
          </div>`
       );
     });
@@ -88,75 +44,27 @@
 
   function showResults() {
     // Export Data - 
-    const answerContainers = quizContainer.querySelectorAll(".answers");
 
     // keep track of user's answers
     let numCorrect = 0;
-
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
   }
 
   function showSlide(n) {
     slides[currentSlide].classList.remove("active-slide");
     slides[n].classList.add("active-slide");
     currentSlide = n;
-
-    if (currentSlide === 0) {
-      previousButton.style.display = "none";
-    } else {
-      previousButton.style.display = "inline-block";
-    }
-
-    if (currentSlide === slides.length - 1) {
-      nextButton.style.display = "none";
-      submitButton.style.display = "inline-block";
-    } else {
-      nextButton.style.display = "inline-block";
-      submitButton.style.display = "none";
-    }
+    submitButton.style.display = "none"
   }
 
   function showNextSlide() {
-    
-    const answerContainer = slides[currentSlide].querySelector(".answers");
-    const selector = `input[name=question${currentSlide}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-    if (!(userAnswer === myQuestions[currentSlide].correctAnswer)){
-   		resultsContainer.innerHTML = `Bad !!! - Will write code to go to next quiz`;
-   		nextButton.style.display = "none"
-   		previousButton.style.display = "none"
-   		submitButton.style.display = "inline-block"
-
-    }else{
-    	showSlide(currentSlide + 1);
-    }
+   	showSlide(currentSlide + 1);
   }
 
   function showPreviousSlide() {
-    showSlide(currentSlide - 1);
+    nextButton.style.display = "none";
+	previousButton.style.display = "none";
+	submitButton.style.display = "inline-block";
+    resultsContainer.innerHTML = `Bad !!! - Will write code to go to next quiz`;
   }
 
   const quizContainer = document.getElementById("quiz");
