b24b664f982ac792ff5258eb17264d3f6cb15d31
Sathyanarayanan Govindarajan <gsathya28@gmail.com>
2018-01-28 11:15:05 -0600

    JS Visual Text Add

--- concussiontest/static/js/visual.js
+++ concussiontest/static/js/visual.js
@@ -20,13 +20,29 @@
     
 
     const output = [];
-    myQuestions.forEach((currentQuestion) => {
-      output.push(
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      if (questionNumber === 0){
+      	output.push(
         // Change to IMG Tag
         `<div class="slide">
         	<img class = question src="${currentQuestion.link}"/>
+        	<p style="color: #fff">Place Demonstration Card in front of subject. The distance should be the distance to the elbow of the subject's arm when his fist rests on his chin. Ask the tester to call out the numbers on the screen as quickly as possible from left to right following the arrows on the demonstration card.</p>
+        	<br>
+        	<p style="color: #fff">When you feel the tester understands the process, start the screening procedure</p>
          </div>`
-      );
+      );	
+      }
+      else
+      {
+      	output.push(
+        // Change to IMG Tag
+        `<div class="slide">
+        	<img class = question src="${currentQuestion.link}"/> 	
+         </div>`
+      );	
+      }
+
+      
     });
 
     // finally combine our output list into one string of HTML and put it on the page
@@ -35,6 +51,7 @@
 
   function showSlide(n) {
     slides[currentSlide].classList.remove("active-slide");
+    
     if (n >= myQuestions.length){
     	nextButton.style.display = "none";
 		document.getElementById("quiz").innerHTML = "You've cleared the Visual Test!"
