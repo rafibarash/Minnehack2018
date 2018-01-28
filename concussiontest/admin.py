c6896c29b7ca5b0b22dece6fe8416d47f65a3225
Rafi Barash <rafabarash@gmail.com>
2018-01-28 01:41:07 -0600

    added athlete model

--- concussiontest/admin.py
+++ concussiontest/admin.py
@@ -4,5 +4,5 @@ from .models import Athlete
 class AthleteAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'slug', 'base_time', 'new_time_1', 'new_time_2')
-    search_fields = ('name')
+    search_fields = ['name']
 admin.site.register(Athlete, AthleteAdmin)
