6bf8cfbd51e14dc5ed0ef81351d1e57b3484635b
Rafi Barash <rafabarash@gmail.com>
2018-01-28 09:36:04 -0600

    no edit error

--- account/urls.py
+++ account/urls.py
@@ -17,11 +17,13 @@ from django.urls import path
 import django.contrib.auth.views as auth_views
 from . import views
 
+app_name='account'
+
 urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
 
     path('register/', views.register, name='register'),
-    path('profile/', views.dashboard, name='dashboard'), # Dashboard
+    path('dashboard/', views.dashboard, name='dashboard'), # Dashboard
     path('edit/', views.edit, name='edit'),
 
     # login / logout urls
